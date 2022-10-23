1ed772bbf4e599e6cae3030676090b9d1d50b2ae
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-10-23 18:08:45 -0400

    added modules and questions to index.js

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application:
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions =
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Whats the title of your project?',
+                name: 'title',
+            },
+
+            {
+                type: 'input',
+                message: 'Whats the description?',
+                name: 'description',
+            },
+
+            {
+                type: 'input',
+                message: 'Whats the instalation instructions?',
+                name: 'installation',
+            },
+
+            {
+                type: 'input',
+                message: 'What is this used for?',
+                name: 'usage',
+            },
+
+            {
+                type: 'input',
+                message: 'Who contributed?',
+                name: 'contributing',
+            },
+
+            {
+                type: 'list',
+                message: 'Choose a license for your project:',
+                name: 'license',
+                choices: ['MIT', 'Apache', 'BSD 2', 'BDS 3', 'GPL', 'LGPL', 'Mozilla', 'Eclipse', 'None']
+            },
+
+            {
+                type: 'input',
+                message: 'What is your GitHub username?',
+                name: 'username',
+            },
+
+            {
+                type: 'input',
+                message: 'What is your email?',
+                name: 'email',
+            },
+
+        ])
+       
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
